ea27e7b59187b94f245baa1f1374a24eec6eca2e
TomCass <qwerto227@gmail.com>
2019-04-11 01:56:41 +0100

    Finished newUser function

--- server.js
+++ server.js
@@ -4,51 +4,55 @@ const port = 3000;
 const authClientID = "0oag1yxo1EKOXWZ5B356";
 const authClientSecret = "INhXV5J2ZZLBhPr1cWi-vzc0q3xpFBsvknhPf4v";
 const authToken = "007siEwzxgmLwaotSw149a4ZTrJ0o-G5nqT5a6-dQB";
+const oktaURL = "https://dev-299193.okta.com";
+const authServ = "https://dev-299193.okta.com/oauth2/default/v1/authorize";
 
 
-var session = require("express-session");
+const session = require("express-session");
 const express = require('express'); //Framework used for server
 const bodyParser = require('body-parser'); //Middleware used to handle JSON, Field Submissions and other data
 const logger = require('morgan'); //Middleware used to keep logs
-const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+const request = require('request');
 
 let app = express();
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 app.use(express.json());
-
-
-
-//The following section manages login using the Okla API wrapper
-const okta = require('@okta/okta-sdk-nodejs'); //Manages login
-const oktaClient = new okta.Client({
-    orgUrl: 'https://dev-299193.okta.com',
-    token: authToken
-});
-const oidc = new ExpressOIDC({
-    issuer: "https://dev-299193.okta.com/oauth2/default",
-    client_id: authClientID,
-    client_secret: authClientSecret,
-    redirect_uri: 'http://localhost:3000/users/callback',
-    appBaseUrl: 'https://dev-299193.okta.com',
-    scope: "openid profile",
-    routes: {
-        login: {
-            path: "/users/login"
-        },
-        callback: {
-            path: "/users/callback",
-            defaultRedirect: "/dashboard"
-        }
-    }
-});
 app.use(session({
     secret: 'wedriudhfiuaYHGHWEIUHDWUEYDFHBWIUHGUIUG88838888888888333PLEASEDONTHACKME uWu',
     resave: true,
     saveUninitialized: false
 }));
 
+function newUser(email, password, charname){  //Creates a new user using the OKTA authentication server
+    let options = {
+        url: oktaURL + '/api/v1/users',
+        json: true,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "SSWS "+authToken
+        },
+        body: {
+
+            profile: {
+                login: email,
+                email: email,
+                displayName: charname
+            },
+            credentials: {
+                "password": password
+            }
+        }
+    };
+    request.post(options, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log("newUSERREQUEST SENT");
+        console.log(body);
+        console.log(body.id);
+    });
+}
 
 app.get('/random/:max', function(req, resp){
     max = parseInt(req.params.max);
@@ -76,22 +80,8 @@ app.post("/signup", function(req, resp){
     console.log("SIGNUP DETECTED BEEP BOOP");
     let email =  req.body.email;
     let password = req.body.password;
-    const newUser = {
-        profile: {
-            email: email,
-            login: email,
-        },
-        credentials: {
-            password : {
-                value: password
-            }
-        }
-    };
-
-     client.createUser(newUser)
-        .then(user => {
-            console.log('Created user', user);
-        }).catch(console.error);
+    let charname = req.body.charname;
+    newUser(email, password, charname);
     console.log("Form received, returning "+ req.body.username);
     resp.redirect('userpage.html');
 
