d3a87b758e0a00cba20d59a90e08c7ecf7db3e78
TomCass <qwerto227@gmail.com>
2019-04-26 01:53:46 +0100

    Did advanced action submission

--- client/userpage.js
+++ client/userpage.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     getCharacter()
 });
 
@@ -7,20 +7,20 @@ const sessionID = Cookies.get("sessionID"); //Uses js-cookie library to manage c
 const userID = Cookies.get("userID");
 
 
-function getCharacter(){
+function getCharacter() {
 
-    axios.get('/char/'+sessionID.toString())
+    axios.get('/char/' + sessionID.toString())
         .then(function (response) {
-        // handle success
-        console.log(response);
-        characterInfo=response.data
+            // handle success
+            console.log(response);
+            characterInfo = response.data;
             populateInfo()
-    })
+        })
         .catch(function (error) {
             // handle error
             console.log(error);
             console.log("CHAR FAILED TO GET, REDIRECTING TO LOGIN");
-            window.location.href = "http://"+window.location.host+"/index.html"
+            window.location.href = "http://" + window.location.host + "/index.html"
         })
         .then(function () {
             // always executed
@@ -32,22 +32,20 @@ function populateInfo() {
     console.log("NAME: " + characterInfo.Name);
     $("#nameinfo").html(characterInfo.Name);
     $("#ppinfo").html(characterInfo.ProgressionPoints);
-    $("#tuinfo").html(characterInfo.TimeUnitsCurrent+"/"+characterInfo.TimeUnitsMax);
+    $("#tuinfo").html(characterInfo.TimeUnitsCurrent + "/" + characterInfo.TimeUnitsMax);
     $("#rpinfo").html(characterInfo.ResearchPoints);
 }
 
-function characterUpdate(updatedvalues){
-    for (var key in updatedvalues){
-        characterInfo[key] = updatedvalues[key]
-    }
+function characterUpdate() { //This updates the servers version of the character sheet with the new one
 
-    axios.post("/updatevalues/"+userID.toString(), characterInfo)
+    axios.post("/updatevalues/" + userID.toString(), characterInfo)
         .then(function (response) {
             populateInfo();
             alert("VALUES SUCCESSFULLY UPDATED")
         })
         .catch(function (error) {
-            alert("Value Update Failed")
+            alert("Value Update Failed");
+            getCharacter()
         })
         .then(function () {
             // always executed
@@ -55,25 +53,51 @@ function characterUpdate(updatedvalues){
 
 }
 
-function studyAction(){
-    if (characterInfo.TimeUnitsCurrent>=2) {
-        characterUpdate({"ResearchPoints":parseInt(characterInfo.ResearchPoints)+20,"TimeUnitsCurrent": characterInfo.TimeUnitsCurrent-2})
-    } else {
-        alert("Not enough Time Units remaining to do this action")
-        }
-}
-
-function trainAction(){
-    if (characterInfo.TimeUnitsCurrent>=5) {
-        characterUpdate({"ProgressionPoints":parseInt(characterInfo.ProgressionPoints)+1,"TimeUnitsCurrent": characterInfo.TimeUnitsCurrent-5})
+function studyAction() { //Triggered when the Study button is pressed
+    if (characterInfo.TimeUnitsCurrent >= 2) { //Check that there are enough resources
+        characterInfo.ResearchPoints += 20;
+        characterInfo.TimeUnitsCurrent -= 2;
+        characterUpdate()
     } else {
         alert("Not enough Time Units remaining to do this action")
     }
 }
-function prepareAction(){
-    if (characterInfo.TimeUnitsCurrent>=2 && characterInfo.ResearchPoints>=10) {
-        characterUpdate({"PreparedMana":parseInt(characterInfo.PreparedMana)+1,"TimeUnitsCurrent": characterInfo.TimeUnitsCurrent-2, "ResearchPoints":characterInfo.ResearchPoints-10})
+
+function trainAction() { //Triggered when the Train button is pressed
+    if (characterInfo.TimeUnitsCurrent >= 5) { //Check that there are enough resources
+        characterInfo.ProgressionPoints += 1;
+        characterInfo.TimeUnitsCurrent -= 5;
+        characterUpdate()
     } else {
         alert("Not enough Time Units remaining to do this action")
+    }
+}
+
+function prepareAction() { //Triggered when the Prepare button is pressed
+    if (characterInfo.TimeUnitsCurrent >= 2 && characterInfo.ResearchPoints >= 10) { //Check that there are enough resources
+        characterInfo.PreparedMana += 1;
+        characterInfo.TimeUnitsCurrent -= 2;
+        characterInfo.ResearchPoints -= 10;
+        characterUpdate()
+    } else {
+        alert("Not enough Time Units remaining to do this action")
+    }
+}
+
+function investigationAction() {
+    if (characterInfo.TimeUnitsCurrent >= 4) {
+        characterInfo.TimeUnitsCurrent -= 4;
+        characterInfo.InvestigationActions.push($('#InvestigationAction').val());
+        characterUpdate()
+    }
+}
+
+
+function researchAction() {
+    if (characterInfo.TimeUnitsCurrent >= 3 && characterInfo.ResearchPoints >= 40) {
+        characterInfo.TimeUnitsCurrent -= 3;
+        characterInfo.ResearchPoints -= 40;
+        characterInfo.ResearchActions.push($('#ResearchAction').val());
+        characterUpdate()
     }
 }
