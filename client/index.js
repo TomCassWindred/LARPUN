d311c39f23a99adf4237d53073864b8f00ca6a6e
TomCass <qwerto227@gmail.com>
2019-04-29 16:21:38 +0100

    LINTed files

--- client/index.js
+++ client/index.js
@@ -1,81 +1,81 @@
-console.log("Index.js started");
+//console.log("Index.js started");
 
 function redirectUserpage(){
-    window.location.href =  "http://"+window.location.host+"/userpage.html"
+	window.location.href =  "http://"+window.location.host+"/userpage.html";
 }
 
 $("#loginform").submit(function(event) {
-    event.preventDefault();
-    // Fetch form to apply custom Bootstrap validation
-    let form = document.querySelector("#loginform");
-    let formData = new FormData(form);
-    event.preventDefault();
-    if (form[0].checkValidity() === false) {
-        event.stopPropagation()
-    }
-    else {
-        axios({
-            method: 'post',
-            url: '/login',
-            data: formData,
-            config: {headers: {'Content-Type': 'multipart/form-data'}}
-        })
-            .then(function (response) {
-                //handle success
-                console.log("LOGIN SUCCESSFUL");
-                console.log(response.data);
-                document.cookie = "sessionID="+response.data.sessionID;
-                document.cookie = "userID="+response.data.userID;
-                redirectUserpage()
+	event.preventDefault();
+	// Fetch form to apply custom Bootstrap validation
+	let form = document.querySelector("#loginform");
+	let formData = new FormData(form);
+	event.preventDefault();
+	if (form[0].checkValidity() === false) {
+		event.stopPropagation();
+	}
+	else {
+		axios({
+			method: "post",
+			url: "/login",
+			data: formData,
+			config: {headers: {"Content-Type": "multipart/form-data"}}
+		})
+			.then(function (response) {
+				//handle success
+				//console.log("LOGIN SUCCESSFUL");
+				//console.log(response.data);
+				document.cookie = "sessionID="+response.data.sessionID;
+				document.cookie = "userID="+response.data.userID;
+				redirectUserpage();
 
-            })
-            .catch(function (response) {
-                //handle error
-                console.log(response);
-                alert("Login Failed, Please ensure your login details are correct")
-            });
+			})
+			.catch(function (response) {
+				//handle error
+				//console.log(response);
+				alert("Login Failed, Please ensure your login details are correct");
+			});
 
 
 
-    }
-    $("#loginform").addClass('was-validated');
+	}
+	$("#loginform").addClass("was-validated");
 });
 
 $("#signupform").submit(function(event) {
-    console.log("SIGNUP DETECTED");
-    event.preventDefault();
-    // Fetch form to apply custom Bootstrap validation
-    let form = document.querySelector("#signupform");
-    let formData = new FormData(form);
-    event.preventDefault();
-    if (form[0].checkValidity() === false) {
-        event.stopPropagation()
-    }
-    else {
+	//console.log("SIGNUP DETECTED");
+	event.preventDefault();
+	// Fetch form to apply custom Bootstrap validation
+	let form = document.querySelector("#signupform");
+	let formData = new FormData(form);
+	event.preventDefault();
+	if (form[0].checkValidity() === false) {
+		event.stopPropagation();
+	}
+	else {
 
-        axios({
-            method: 'post',
-            url: '/signup',
-            data: formData,
-            config: {headers: {'Content-Type': 'multipart/form-data'}}
-        })
-            .then(function (response) {
-                //handle success
-                console.log("RESPONSE RECEIVED");
-                console.log(response);
-                alert("USER CREATED YOU MAY NOW LOG IN")
-            })
-            .catch(function (error) {
-                //handle error
+		axios({
+			method: "post",
+			url: "/signup",
+			data: formData,
+			config: {headers: {"Content-Type": "multipart/form-data"}}
+		})
+			.then(function (response) {
+				//handle success
+				//console.log("RESPONSE RECEIVED");
+				//console.log(response);
+				alert("USER CREATED YOU MAY NOW LOG IN");
+			})
+			.catch(function (error) {
+				//handle error
 
-                console.log("ERROR :(");
-                console.log(error.response.data);
-                console.log("RECEIVED RESPONSE: "+error.response.data);
-                alert(error.response.data)
-            });
+				//console.log("ERROR :(");
+				//console.log(error.response.data);
+				//console.log("RECEIVED RESPONSE: "+error.response.data);
+				alert(error.response.data);
+			});
 
 
 
-    }
-    $("#signupform").addClass('was-validated');
+	}
+	$("#signupform").addClass("was-validated");
 });
